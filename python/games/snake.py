d6bb4ab93d833168adb9a4582063e447aa441f1a
Marko Echevarria <markofidel10@gmail.com>
2025-11-06 14:20:42 -0500

    Update snake.py

--- python/games/snake.py
+++ python/games/snake.py
@@ -3,7 +3,7 @@ import time
 import random
 import keyboard
 
-length=20
+last_key="l"
 
 def create_table(length):
     return [ ["." for j in range(length)] for i in range(length)]
@@ -17,45 +17,71 @@ def create_player(table):
     }
     return player
 
-def print_table_with_player(table, player): 
+def print_table_with_player(table, player):
     for i in range(len(table)):
         for j in range(len(table)):
             value=True
             for key in player.keys():
-                if player[key] == [i,j]: 
+                if player[key] == [i,j]:
                     value=False
                     print("■", end=" ")
             if value: print( table[i][j], end=" ")
         print("")
 
+def get_last_key(event):
+    global last_key
+    last_key = event.name
+
 def detect_key():
-    pressed = keyboard.read_key()
-    if pressed == "h":
+    move(last_key, player)
+    """
+    if last_key == "h":
+        move("h", player)
+    if last_key == "j":
+        move("j", player)
+    if last_key == "k":
+        move("k", player)
+    if last_key == "l":
+        move("l", player)
+    else: move(last_key, player)
+    """
+
+def move(key, player):
+    if key == "h":
         for i in range(len(player), 0, -1):
-            if i == 1: player[i][1] = player[i][1]+1
-            else: player[i] = player[i-1] 
-    if pressed == "j":
+            if i == 1: player[i][1] = player[i][1]-1
+            else: player[i] = player[i-1].copy()
+
+    if key == "j":
         for i in range(len(player), 0, -1):
             if i == 1: player[i][0] = player[i][0]+1
-            else: player[i] = player[i-1] 
-    if pressed == "k":
+            else: player[i] = player[i-1].copy()
+
+    if key == "k":
         for i in range(len(player), 0, -1):
             if i == 1: player[i][0] = player[i][0]-1
-            else: player[i] = player[i-1] 
-    if pressed == "l":
-        for i in range(len(player), 0, -1):
-            print("para debugear", i, player[i])
-            if i == 1: player[i][1] = player[i][1]+1
-            else: player[i] = player[i-1] 
+            else: player[i] = player[i-1].copy()
 
-table = create_table(length)
+    if key == "l":
+        for i in range(len(player), 0, -1):
+            if i == 1: player[i][1] = player[i][1]+1
+            else: player[i] = player[i-1].copy()
+
+def award():
+    i = len(player)
+    while ( i<len(player)):
+        prize = [ random.randint(1, len(table)), random.randint(1, len(table)) ]
+        if prize[1] == prize[i][1] and prize[0] == player[i][0]: i=i-1
+    return prize
+
+table = create_table(length=30)
 player = create_player(table)
+apple = award()
 
 for i in range(20000):
     os.system("clear")
     print( f"Iteration {i}")
     print_table_with_player(table, player)
     detect_key()
-    player = dict(sorted(player.items()))
-    print(player)
-    time.sleep(3.5)
+    keyboard.on_press(get_last_key)
+    time.sleep(0.05)
