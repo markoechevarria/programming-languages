1462ed26ebcca6dd7480175c02bdd2d854f48358
Marko Echevarria <markofidel10@gmail.com>
2025-11-07 21:41:43 -0500

    finishing snake game

--- python/games/snake.py
+++ python/games/snake.py
@@ -3,10 +3,8 @@ import time
 import random
 import keyboard
 
-last_key="l"
-
 def create_table(length):
-    return [ ["." for j in range(length)] for i in range(length)]
+    return [ ["." if j==0 or j==length-1 or i==0 or i==length-1 else " " for j in range(length)] for i in range(length)]
 
 def create_player(table):
     player = {
@@ -17,10 +15,13 @@ def create_player(table):
     }
     return player
 
-def print_table_with_player(table, player):
+def print_table_with_player(table, player, prize):
     for i in range(len(table)):
         for j in range(len(table)):
             value=True
+            if prize[0]== i and prize[1]== j :
+                print("#", end="")
+                value=False
             for key in player.keys():
                 if player[key] == [i,j]:
                     value=False
@@ -30,21 +31,16 @@ def print_table_with_player(table, player):
 
 def get_last_key(event):
     global last_key
-    last_key = event.name
-
-def detect_key():
-    move(last_key, player)
-    """
-    if last_key == "h":
-        move("h", player)
-    if last_key == "j":
-        move("j", player)
-    if last_key == "k":
-        move("k", player)
-    if last_key == "l":
-        move("l", player)
-    else: move(last_key, player)
-    """
+    if last_key == "h" and event.name == "l":
+        last_key = "h"
+    if last_key == "l" and event.name == "h":
+        last_key = "l"
+    if last_key == "j" and event.name == "k":
+        last_key = "j"
+    if last_key == "k" and event.name == "j":
+        last_key = "k"
+    else:
+        last_key = event.name
 
 def move(key, player):
     if key == "h":
@@ -67,21 +63,47 @@ def move(key, player):
             if i == 1: player[i][1] = player[i][1]+1
             else: player[i] = player[i-1].copy()
 
-def award():
-    i = len(player)
-    while ( i<len(player)):
-        prize = [ random.randint(1, len(table)), random.randint(1, len(table)) ]
-        if prize[1] == prize[i][1] and prize[0] == player[i][0]: i=i-1
+def eat(prize, player, puntaje):
+    if player[1] == prize:
+        player[len(player)+1] = player[len(player)].copy()
+        prize = award(player)
+        puntaje=puntaje+1
+    return player, prize, puntaje
+
+def award(player):
+    i = 1
+    print("first player", player, " count: ", len(player))
+    while ( i < len(player)):
+        prize = [ random.randint(1, len(table)-2), random.randint(1, len(table)-2) ]
+        if prize[1] == player[i][1] and prize[0] == player[i][0]: i=i-1
+        i = i+1
     return prize
 
+def check_dead(player, table):
+    l = len(table)-1
+    if player[1][0] == l or player[1][1] == l or player[1][0] == 0 or player[1][1] == 0:
+        print("===============================")
+        print("========   GAME OVER   ========")
+        print("===============================")
+        return True
+    return False
+
+last_key="l"
 table = create_table(length=30)
 player = create_player(table)
-apple = award()
+apple = award(player)
+puntaje = 0
 
 for i in range(20000):
     os.system("clear")
-    print( f"Iteration {i}")
-    print_table_with_player(table, player)
-    detect_key()
+    print("===============================")
+    print( f"======    SNAKE GAME    =======")
+    print("===============================")
+    print( f"======    PUNTAJE {puntaje}    =======")
+    print("===============================")
+    print_table_with_player(table, player, apple)
+    move(last_key, player)
+    player, apple, puntaje = eat(apple, player, puntaje)
+    if check_dead(player, table): break
     keyboard.on_press(get_last_key)
-    time.sleep(0.05)
+    time.sleep(0.1)
