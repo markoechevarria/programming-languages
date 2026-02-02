0a887b72e1ab21c119fa64f8e405d19faa46d6f7
markoechevarria <markofidel10@gmail.com>
2026-02-01 21:49:40 -0500

    generic, traits and lifetimes

--- rust/r10_generic_types_traits_and_lifetimes/src/main.rs
+++ rust/r10_generic_types_traits_and_lifetimes/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Debug};
+
 struct Point<T> {
     x: T,
     y: T
@@ -34,6 +36,81 @@ enum Result<T, E> {
     Err(E)
 }
 
+pub trait Summary {
+    fn summarize(&self) -> String;
+
+    fn summarize_default(&self) -> String{
+        String::from("(Read more...)")
+    }
+
+    fn summarize_author(&self) -> String;
+
+    fn summarize_more(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+
+pub struct NewArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String
+}
+
+impl Summary for NewArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location) 
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
+    }
+}
+
+pub struct SocialPost {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub repost: bool
+}
+
+impl Summary for SocialPost {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content) 
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("{}", self.username)
+    }
+}
+
+struct Pair<T> {
+    x: T,
+    y: T
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+// impl<T: Display> ToString for T {}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str
+}
 
 fn main() {
     let char_list = vec!['a', 'b', 'c', 'd', 'e'];
@@ -46,6 +123,21 @@ fn main() {
     let _integer = Point { x: 5, y: 10 };
     let _float = Point { x: 1.0, y: 4.0 };
     let _integer_and_float = Points { x: 1.0, y: 3 };
+
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+    }
+    // println!("The longest string is {result}!");
+    
+    let novel = String::from("call me Ishmael. Some years ago ...");
+    let first_sentence = novel.split('.').next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    }
+    
 }
 
 fn largest_char(list: &[char]) -> &char {
@@ -72,3 +164,103 @@ fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
 
     largest
 }
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news: {}", item.summarize());
+}
+
+pub fn notify2<T: Summary>(item: &T) {
+    println!("Breaking news: {}", item.summarize());
+}
+
+pub fn notify3(item1: &impl Summary, item2: &impl Summary) {}
+
+pub fn notify4<T: Summary>(item1: &T, item2: &T) {}
+
+pub fn notify5(item: &(impl Summary + Display)) {}
+
+pub fn notify6<T: Summary + Display>(item: &T) {}
+
+pub fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) {}
+
+pub fn some_function2<T, U>( t: &T, u: &U) -> i32
+where 
+    T: Display + Clone,
+    U: Clone + Debug,
+{ 0 }
+
+pub fn returns_summarizable() -> impl Summary {
+    SocialPost {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        repost: false
+    }
+}
+
+/*
+fn returns_summarizable2(switch: bool) -> impl Summary {
+    if switch {
+        NewArticle {
+            headline: String::from(
+                "Penguins win the Stanley Cup Championship!",
+            ),
+            location: String::from("Pittsburgh, PA, USA"),
+            author: String::from("Iceburgh"),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best \
+                 hockey team in the NHL.",
+            ),
+        }
+    } else {
+        SocialPost {
+            username: String::from("horse_ebooks"),
+            content: String::from(
+                "of course, as you probably already know, people",
+            ),
+            reply: false,
+            repost: false,
+        }
+    }
+}
+*/
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() { x } else { y }
+}
+
+fn longest2<'a>(x: &'a str, y: &str) -> &'a str {
+    x
+}
+
+/*
+fn longest3<'a>( x: &str, y: &str) -> &'a str {
+    let result = String::from("really long string");
+    result.as_str()
+}
+*/
+
+fn first_word( s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {ann}");
+    if x.len() > y.len() { x } else { y }
+}
+
